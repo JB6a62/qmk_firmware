820913019579a00b1305b89812fb7aa183a67289
6a62 <6a62@protonmail.com>
2020-01-22 10:02:59 +0000

    Add media layer

--- keyboards/macro20/keymaps/default/keymap.c
+++ keyboards/macro20/keymaps/default/keymap.c
@@ -6,7 +6,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_P7,   KC_P8,   KC_P9,   KC_PMNS,
     KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
     KC_P1,   KC_P2,   KC_P3,   KC_BSPC,
-		KC_NO,    KC_P0,   KC_PDOT, KC_ENT
+		TG(1),   KC_P0,   KC_PDOT, KC_ENT
+  ),
+
+	[1] = LAYOUT_ortho_5x4( /* Media */
+    KC_NO,   KC_NO,   KC_NO,   KC_MUTE,
+    KC_NO,   KC_NO,   KC_NO,   KC_VOLD,
+    KC_NO,   KC_NO,   KC_NO,   KC_VOLU,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,
+		KC_TRNS, KC_NO,   KC_NO,   KC_NO
   ),
 };
 
